2737451517930df8e2a3769bd0666d18fc952bad
Marinho3104 <mariovieira2003@gmail.com>
2023-07-28 17:40:48 +0100

    Setup everything up

--- example/main.cu
+++ example/main.cu
@@ -1,9 +1,6 @@
 
 /* INCLUDES */
 
-// Own
-#include "field_element_cuda.cuh" // struct Field_Element
-
 // Nvcc
 #include <cuda_runtime.h>
 #include <iostream>
@@ -11,17 +8,10 @@
 
 __global__ void test_field_element() {
 
-    // Field Elements creation
-    printf("Hello, World! From GPU\n");
-
 }
 
 int main() {
 
-    // Tests about field element
-    test_field_element<<< 1, 1 >>>();
-
-    // Synchronize cuda threads
-    cudaDeviceSynchronize();
+    
 
 }
